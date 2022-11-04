34a609ca19e703ee7531bd036e95ca711edb8bc4
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-11-04 21:30:59 +0000

    router demo is now working

--- 111-multi-page-app/my-app1/src/App.js
+++ 111-multi-page-app/my-app1/src/App.js
@@ -1,56 +1,41 @@
 import React from "react";
-import { Switch } from "react-router";
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Route,
+    Routes,
 } from "react-router-dom";
 
 export default function App() {
-  return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+           <Routes>
+                <Route
+                    path="/about"
+                    element={<About/>}
+                />
+                <Route
+                    path="/users"
+                    element={<Users/>}
+                />
+                <Route
+                    path="/"
+                    element={<Home/>}
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 function Home() {
-  return <h2>Home</h2>;
+    return <h2>Home</h2>;
 }
 
 function About() {
-  return <h2>About</h2>;
+
+    return <h2>About</h2>;
 }
 
 function Users() {
-  return <h2>Users</h2>;
+    return <h2>Users</h2>;
 }
