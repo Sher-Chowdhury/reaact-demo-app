efa3e7dde84b539de3329f4c5cd37c7deaef56b7
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-12-10 23:45:42 +0000

    wip

--- 112-server-side-rendering-with-express/my-app1/webpack.config.js
+++ 112-server-side-rendering-with-express/my-app1/webpack.config.js
@@ -1,9 +1,10 @@
-const path = require("path");
-module.exports = {
+import path from "path"
+
+const config = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(process.cwd(), 'dist'),
     },
     module: {
         rules: [
@@ -17,3 +18,5 @@ module.exports = {
         ],
     },
 };
+
+export {config}
