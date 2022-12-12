9cabd85f901fa537de4f1a12c84efd33c2e126a7
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-12-12 22:39:36 +0000

    wip

--- 112-server-side-rendering-with-express/my-app2-csr-with-backend/src/routes/petstoreRoutes.js
+++ 112-server-side-rendering-with-express/my-app2-csr-with-backend/src/routes/petstoreRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express')
 
-const app = express()
+// const app = express()
+//
+// // This get basically means if this endpoint is requested, then run this javascript arrow function:
+// app.get('/api/v1/greetings', (req, res) => {
+//     res.send('hello from /api/v1/greetings endpoint')
+// })
 
-// This get basically means if this endpoint is requested, then run this javascript arrow function:
-app.get('/api/v1/greetings', (req, res) => {
+
+const router = express.Router()
+
+router.get('/api/v1/greetings', (req, res) => {
     res.send('hello from /api/v1/greetings endpoint')
 })
+
+module.exports = router
