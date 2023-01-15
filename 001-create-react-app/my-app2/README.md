a33a2741116bf2f90d4a81c300a1bf21bfdf86dd
Sher Chowdhury <sher.chowdhury@ibm.com>
2023-01-15 00:10:58 +0000

    wip

--- 001-create-react-app/my-app2/README.md
+++ 001-create-react-app/my-app2/README.md
@@ -10,6 +10,15 @@ The main packages are:
 In this example, nnce that express app delivered teh initial html file + bundle.js, it plays no further part, i.e. all future
 page loads and handled by the bundle.js file on the client side, hence this app is an example of a client-side-rendered (csr) app. 
 
+To run this demo, do:
+
+```shell
+npm run dev:bundler
+npm start
+```
+
+The `dev:bundler` script is creates the bundle.js file, which is optimised to make it easier for debugging, e.g. not minified. code.
+
 
 
 IGNORE THE REST OF THIS PAGE FOR NOW:
--- 001-create-react-app/my-app2/src/components/index.js
+++ 001-create-react-app/my-app2/src/components/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
-import {useState} from 'react';
 import ReactDOM from 'react-dom/client'; // This I think indicates that this app is a client-side-rendered (csr) app
 
 
-function Counter(props) {
-
-    const [counter, setCounter] = useState(0);
-
-    // Functions that are to be called by eventHandlers e.g. onCLick, should be stored in a variable.
-    const IncrementCounter = ()=> {
-        // calling useState() generated functions will automtically trigger a new render.
-        setCounter(count => count + 1)
-    }
-    return (
-        <div>
-            <h1>Hello, {props.firstName} {props.lastName}</h1>
-            <button onClick={IncrementCounter}>{counter}</button>
-        </div>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<Counter firstName="Peter" lastName="Parker" />);
+root.render(<p>Hello World!!!</p>);
